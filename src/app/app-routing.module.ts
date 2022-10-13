8f409692d736b5beded00c7cd7556710e5d49730
Ayça Yiğit <115551844+sednaProje@users.noreply.github.com>
2022-10-13 10:08:54 +0300

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatToolbar } from '@angular/material/toolbar';
+import { LoginComponent } from './Pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
